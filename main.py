8fa4ecd841bcb1c477f91437cbea8c71d03d714b
Danny <39076480+denemir@users.noreply.github.com>
2025-01-10 00:53:28 -0600

    1.0.1
    Re-enabled sleep functionality

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ class App:
         system = platform.system()
         try:
             if system == "Windows":
-                print()
-                # os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+                os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
             elif system == "Darwin":
                 subprocess.run(["pmset", "sleepnow"])
             elif system == "Linux":
